9d828b8bc11e73436184f15cf9228f05b4ee422f
sahilkhurana19 <sahil_khurana@outlook.com>
2017-08-26 02:38:33 +0530

    Added users model

--- app/models.py
+++ app/models.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django import forms
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    password = models.CharField(max_length = 100)
+    number = models.IntegerField()
+    organisation = models.CharField(max_length = 100)
